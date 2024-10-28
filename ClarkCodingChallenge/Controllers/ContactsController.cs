d49a01c43bb5f17b367ea1b5591a43c8ee8ede81
Nolan Williams <ntwilliams18@gmail.com>
2024-10-27 20:04:12 -0400

    Added Create contact UI and controller actions w/ confirmation

--- ClarkCodingChallenge/Controllers/ContactsController.cs
+++ ClarkCodingChallenge/Controllers/ContactsController.cs
@@ -1,14 +1,50 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using ClarkCodingChallenge.Models;
+using ClarkCodingChallenge.DataAccess;
+using ClarkCodingChallenge.DataMapping;
 
 namespace ClarkCodingChallenge.Controllers
 {
     public class ContactsController : Controller
     {
-        public IActionResult Index()
+        #region Properties
+
+        private readonly ContactsDataAccess contactsDataAccess;
+
+        #endregion
+
+        #region Construction
+
+        public ContactsController(ContactsDataAccess contactsDataAccess)
+		{
+            this.contactsDataAccess = contactsDataAccess;
+		}
+
+        #endregion
+
+        #region Actions
+
+        public IActionResult Index() => RedirectToAction("Create");
+
+        public IActionResult Create()
         {
-            return View();
+            return View(new ContactViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult Create(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+				this.contactsDataAccess.SaveContact(ContactDataMapper.ToEntity(model));
+				this.addConfirmationMessage($"Contact {model.FullName} created successfully.");
+                return RedirectToAction(nameof(Create));
+            }
+            else
+            {
+                return View(model);
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
@@ -16,5 +52,16 @@ namespace ClarkCodingChallenge.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        #endregion
+
+        #region Private Helpers
+
+        private void addConfirmationMessage(string message)
+        {
+            TempData["ConfirmationMessage"] = message;
+        }
+
+        #endregion
     }
 }
--- ClarkCodingChallenge/Startup.cs
+++ ClarkCodingChallenge/Startup.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using ClarkCodingChallenge.DataAccess;
+using ClarkCodingChallenge.DataAccess.Implementations;
+using ClarkCodingChallenge.DataAccess.Interfaces;
+using ClarkCodingChallenge.Models.Entities;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -20,15 +24,16 @@ namespace ClarkCodingChallenge
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
+			services.Configure<CookiePolicyOptions>(options =>
+			{
+				// This lambda determines whether user consent for non-essential cookies is needed for a given request.
+				options.MinimumSameSitePolicy = SameSiteMode.None;
+			});
 
+			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            services.AddSingleton<IRepository<ContactEntity>, InMemoryRepository<ContactEntity>>();
+            services.AddTransient<ContactsDataAccess>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
